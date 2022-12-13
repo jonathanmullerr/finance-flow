6cbf25fe21e7102a82abeab2e38e2c19515930ef
Jonathan <jonathanfelipe.muller@gmail.com>
2022-12-13 11:28:11 -0300

    adding routes and actions

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,44 @@
 class ExpensesController < ApplicationController
+  before_action :set_expense, only: [:show, :update, :destroy]
+
+  def index
+    @expenses = Transaction::Expense.all
+
+    render json: @expenses
+  end
+
+  def show
+    render json: @expense
+  end
+
+  def create
+    @expense = Transaction::Expense.new(expense_params)
+
+    if @expense.save
+      render json: @expense, status: :created, location: @expense
+    else
+      render json: @expense.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @expense.update(expense_params)
+      render json: @expense
+    else
+      render json: @expense.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @expense.destroy
+  end
+
+  private
+    def set_expense
+      @expense = Transaction::Expense.find(params[:id])
+    end
+
+    def expense_params
+      params.fetch(:expense, {})
+    end
 end
--- test/controllers/incomings_controller_test.rb
+++ test/controllers/incomings_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class IncomingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @incoming = incomings(:one)
+  end
+
+  test "should get index" do
+    get incomings_url, as: :json
+    assert_response :success
+  end
+
+  test "should create incoming" do
+    assert_difference('Incoming.count') do
+      post incomings_url, params: { incoming: {  } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show incoming" do
+    get incoming_url(@incoming), as: :json
+    assert_response :success
+  end
+
+  test "should update incoming" do
+    patch incoming_url(@incoming), params: { incoming: {  } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy incoming" do
+    assert_difference('Incoming.count', -1) do
+      delete incoming_url(@incoming), as: :json
+    end
+
+    assert_response 204
+  end
 end
