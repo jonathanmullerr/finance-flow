b27787481f4134321407437e0ebcbb66d49219e8
Jonathan <jonathanfelipe.muller@gmail.com>
2022-12-15 18:12:05 -0300

    add category controller + tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  validates :name, presence: true
+
   has_many :entry_categories
   has_many :entries, through: :entry_categories
 end
--- spec/models/entry_category_spec.rb
+++ spec/models/entry_category_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe EntryCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:entry) }
+    it { is_expected.to belong_to(:category) }
+  end
 end
